99363e36d61c40c4ec31085cf54840ddca8c089a
Vishal Vaishnav <vishalvaishnav2677@gmail.com>
2026-01-19 00:00:56 +0530

    jsp tuts [VV]

---
+++ src/main/java/com/jpaTutorial/jpaTuts/JpaTutorialApplication.java
@@ -0,0 +1,14 @@
+package com.jpaTutorial.jpaTuts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaTutorialApplication {
+
+	public static void main(String[] args) {
+		//start project
+		SpringApplication.run(JpaTutorialApplication.class, args);
+	}
+
+}
